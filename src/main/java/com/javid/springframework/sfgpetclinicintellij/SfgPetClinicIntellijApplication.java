ba65b2333a9ab213e887f53567dd45b8abdf3566
Javid Rafiee <javid.rafiee@gmail.com>
2020-12-21 17:24:34 +0330

    Initial Project Add

---
+++ src/main/java/com/javid/springframework/sfgpetclinicintellij/SfgPetClinicIntellijApplication.java
@@ -0,0 +1,13 @@
+package com.javid.springframework.sfgpetclinicintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicIntellijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicIntellijApplication.class, args);
+    }
+
+}

cf8e08101e0a774d75baaa405b78c7eb0c382f31
javaj-r <javid.rafiee@gmail.com>
2021-01-07 18:42:30 +0330

    Adding base entity. Closes #9.

---
+++ pet-clinic-data/src/main/java/com/javid/springframework/sfgpetclinicintellij/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.javid.springframework.sfgpetclinicintellij.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/com/javid/springframework/sfgpetclinicintellij/model/Person.java
+++ pet-clinic-data/src/main/java/com/javid/springframework/sfgpetclinicintellij/model/Person.java
@@ -1,6 +1,6 @@
 package com.javid.springframework.sfgpetclinicintellij.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 

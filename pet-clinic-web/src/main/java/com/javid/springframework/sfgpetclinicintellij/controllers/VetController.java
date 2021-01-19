ad7201df6530d48ac35d972ccba539d48030fe95
javaj-r <javid.rafiee@gmail.com>
2021-01-20 00:22:39 +0330

    Adding Vets list and updating VetController. Closes #14.

--- pet-clinic-web/src/main/java/com/javid/springframework/sfgpetclinicintellij/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/javid/springframework/sfgpetclinicintellij/controllers/VetController.java
@@ -1,14 +1,24 @@
 package com.javid.springframework.sfgpetclinicintellij.controllers;
 
+import com.javid.springframework.sfgpetclinicintellij.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String vets() {
+    public String vets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }

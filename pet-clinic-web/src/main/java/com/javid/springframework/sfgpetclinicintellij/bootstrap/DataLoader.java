3f693d29b61ea6778143bb21470dabc3001fa01d
javaj-r <javid.rafiee@gmail.com>
2021-01-28 16:34:10 +0330

    Adding PetTypes to bootstrap. Closes #31.

--- pet-clinic-web/src/main/java/com/javid/springframework/sfgpetclinicintellij/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/javid/springframework/sfgpetclinicintellij/bootstrap/DataLoader.java
@@ -2,8 +2,10 @@ package com.javid.springframework.sfgpetclinicintellij.bootstrap;
 
 import com.javid.springframework.sfgpetclinicintellij.model.Owner;
 import com.javid.springframework.sfgpetclinicintellij.model.Person;
+import com.javid.springframework.sfgpetclinicintellij.model.PetType;
 import com.javid.springframework.sfgpetclinicintellij.model.Vet;
 import com.javid.springframework.sfgpetclinicintellij.services.OwnerService;
+import com.javid.springframework.sfgpetclinicintellij.services.PetTypeService;
 import com.javid.springframework.sfgpetclinicintellij.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,14 +15,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = petTypeService.save(setPetType("Dog"));
+
+        PetType cat = petTypeService.save(setPetType("Cat"));
+
+        PetType hamster = petTypeService.save(setPetType("Hamster"));
+
+        PetType canary = petTypeService.save(setPetType("Canary"));
+
+        System.out.println("Loaded PetTypes......");
 
         Owner owner1 =  setPersonParam(new Owner(), "Michael", "Weston");
         ownerService.save(owner1);
@@ -44,6 +58,12 @@ public class DataLoader implements CommandLineRunner {
         t.setFirstName(fName);
         t.setLastName(lName);
         return t;
+    }
+
+    private PetType setPetType(String name) {
+        PetType petType = new PetType();
+        petType.setName(name);
+        return petType;
     }
 
 }

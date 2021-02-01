af644843915204fead0100f7c95c2c5985d8183b
javaj-r <javid.rafiee@gmail.com>
2021-02-01 10:29:34 +0330

    Updating Owner Entity with JPA mappings. Closes #35.

--- pet-clinic-data/src/main/java/com/javid/springframework/sfgpetclinicintellij/model/Owner.java
+++ pet-clinic-data/src/main/java/com/javid/springframework/sfgpetclinicintellij/model/Owner.java
@@ -1,13 +1,23 @@
 package com.javid.springframework.sfgpetclinicintellij.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephon")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/com/javid/springframework/sfgpetclinicintellij/model/Pet.java
+++ pet-clinic-data/src/main/java/com/javid/springframework/sfgpetclinicintellij/model/Pet.java
@@ -1,12 +1,24 @@
 package com.javid.springframework.sfgpetclinicintellij.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_type_id")
     private PetType type;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "name")
     private String name;
 
     public LocalDate getBirthDate() {
